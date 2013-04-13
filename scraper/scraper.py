20646e16168c48165df55763959e989474bf838b
llovett <llovett@oberlin.edu>
2013-04-13 15:52:28 -0400

    fixing some things with parsing recipes

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,9 @@
-import urllib, urllib2
+import urllib2
+import cPickle
 from bs4 import BeautifulSoup
 
+# All the ingredients we've found while crawling
+INGREDIENTS = set()
 # Number of pages to search through for each recipe category
 MAX_PAGES = 1
 # Shows a page of salad recipes
@@ -15,29 +18,39 @@ def parseRecipe(url):
     '''
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page)
-    ingredients_html = soup.find_all(class_='fl-ing')
+    ingredients_html = soup.find_all(id='liIngredient')
     ingredients = []
     for ingredient in ingredients_html:
+        ing_amnt = float(ingredient.get('data-grams'))
+        # Ignore "empty" ingredients... don't know why these are in the HTML.
+        if ing_amnt <= 0:
+            continue
         ing_name = ingredient.find(class_='ingredient-name')
-        # This may not exist, for things like "Black pepper to taste"
-        ing_amnt = ingredient.find(class_='ingredient-amount')
+        INGREDIENTS.add(ing_name)
         # Assume if there is no ingredient amount that the amount is very small
-        ingredients.append( (ing_amnt.string if ing_amnt else 'a dash',
-                             ing_name.string) )
+        ingredients.append( (ing_amnt, ing_name.string) )
     return ingredients
+
+def parseListing(url, pagenum):
+    '''Parses a listing of recipes given at a <url> at page <pagenum>'''
+    url = URL%pagenum
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page)
+    recipes = soup.find_all('div', class_='recipes')
+    for recipe in recipes:
+        # Ignore unrated/low-rated recipes
+        rating = recipe.find(itemprop='ratingValue').get('content')
+        if not rating or float(rating) < 4.0:
+            continue
+        rlink = recipe.find('div',class_='rectitlediv').h3.a.get('href')
+        print rating, rlink
+        print parseRecipe(rlink)
 
 if __name__ == '__main__':
     for i in xrange(1,MAX_PAGES+1):
-        url = URL%i
-        page = urllib2.urlopen(url)
-        soup = BeautifulSoup(page)
-        recipes = soup.find_all('div', class_='recipes')
-        for recipe in recipes[:5]:
-            # Ignore unrated/low-rated recipes
-            rating = recipe.find(itemprop='ratingValue').get('content')
-            if not rating or float(rating) < 4.0:
-                continue
-            rlink = recipe.find('div',class_='rectitlediv').h3.a.get('href')
-            print rating, rlink
-            print parseRecipe(rlink)
+        parseListing(URL, i)
+
+    # Pickle our list of ingredients
+    with open("ingredients_list.dat","wb") as pickled:
+        cPickle.dump(list(INGREDIENTS),pickled,protocol=2)
 
